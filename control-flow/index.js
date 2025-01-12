c32470a21a3217b6eaff903610b2be9c15b06265
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2025-01-12 08:18:51 +0000

    complete tell me the day challenge

--- control-flow/index.js
+++ control-flow/index.js
@@ -1,68 +1,31 @@
-// Control Flow
-// Why Do we use control flow in our apps?
-// How do we use it?
-// What patterns can we use?
+//Challenge: Tell me the day!
+let day = 7;
 
-// if / else
-
-// if([expression to evaluate]) {
-//      block of code to execute when expression is true
-// } else {
-//      block of code to execute when all previous expressions were false
-// }
-
-const correctPassword = 'poiqeroiehfkjasdf';
-let input = 'poiqeroiehfkjasdf';
-input = 'polo';
-
-if (input === correctPassword) {
-    console.log('welcome');
-} else if (input === '') {
-    console.log('please input password');
-} else {
-    console.log('access denied');
-}
-
-// Switch
-// cases
-// default
-
-const errorCode = 4; // errorCode - can be 1 to 3 - 0 is success
-
-switch (errorCode) {
-    case 0:
-        console.log('Success');
-        break;
+switch (day) {
     case 1:
-        console.log('try again');
-
+        console.log("Today is Monday");
         break;
     case 2:
-    case undefined:
-    case 'two':
-    case '2':
-        console.log('disk error');
+        console.log("Today is Tuesday");
         break;
     case 3:
-        console.log('Command not recognised');
+        console.log("Today is Wednesday");
         break;
     case 4:
-        console.log('4');
+        console.log("Today is Thursday");
         break;
-
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+        break;
     default:
-        console.log('Error not valid');
+        console.log("Please enter a number between 1 and 7.");
 }
 
-// Ternary operators
+//Challenge: Eye Colour
 
-const score = 10;
-const target = 15;
-// [expression to evaluate] ? [value to return if true] : [value to return if false]
-const qualificationStatus =
-    score > target ? "You're qualified" : "You've NOT qualified";
-
-console.log(qualificationStatus);
-
-const number = 5 > 6 ? '5 is greater than 6' : '5 is Not greater than 6';
-console.log(number);
